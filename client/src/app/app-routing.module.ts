295c39f0d6065efb44913c35e9e27f966929b808
shiyun-ho <serene.hoshiyun@gmail.com>
2023-07-01 17:25:56 +0800

    resolve OktaAuth module issue with deployment for CORS

--- client/src/app/app-routing.module.ts
+++ client/src/app/app-routing.module.ts
@@ -15,7 +15,7 @@ const oktaConfig =
   {clientId: '0oaa4vovf6CwjcZVQ5d7',
   issuer: 'https://dev-69693914.okta.com/oauth2/default',
   // redirectUri: 'http://localhost:4200/login/callback',
-  redirectUri: 'https://nus-capstone-project-shiyun-ho-s-team.vercel.app/login/callback',
+  redirectUri: 'https://nus-capstone-project-r7gmx0rns-shiyun-ho-s-team.vercel.app/login/callback',
   scopes: ['openid', 'profile', 'email']};
 const oktaAuth = new OktaAuth(oktaConfig);
 const moduleConfig: OktaConfig = { oktaAuth };
